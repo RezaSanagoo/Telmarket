c1448031b576549259db02372a8514dda94fe145
RezaSanagoo <rezasanagoo021@gmail.com>
2025-01-09 15:31:06 +0330

    search fix

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,25 +1,25 @@
 import Search from "@mui/icons-material/Search";
 
 export default function SearchPage() {
-  const searchItems = ['بازار طلا', 'رمز ارز', 'فارکس', 'بورس', 'قیمت دلار'];
-  const searchIcon = <Search sx={{ fontSize: 18 }} />;
+  const searchItems = ['بازار طلا و دلار', 'رمز ارز', 'بیت‌کوین', 'بورس و اوراق', 'فارکس'];
+  const searchIcon = <Search sx={{ fontSize: 20, color: "#003988" }} />;
 
 
   return (
-    <div className="flex flex-col items-center w-full p-6 gap-[40px]">
+    <div className="flex flex-col items-center w-full px-3 py-4 gap-8">
       {/* Search Bar */}
-      <div className="relative w-full h-[40px] mt-4">
+      <div className="relative w-full h-[40px]">
         <div className="flex items-center w-full h-full px-4 bg-[#ECECEC] rounded-[9px] gap-4">
-          <Search className="w-[18.5px] h-[18.5px] text-gray-500" />
+          <Search className=" text-gray-500" />
           <input
             type="text"
             placeholder="جستجو"
-            className="w-[121px] h-[20px] bg-transparent outline-none font-iranYekan text-right placeholder:text-gray-500"
+            className="w-full h-[20px] bg-transparent outline-none font-iranYekan text-right placeholder:text-gray-500"
           />
         </div>
       </div>
 
-      {/* Popular Searches Title */}
+    <div className="flex flex-col gap-5 w-full">
       <div className="w-full h-[27px] font-iranYekan text-[16px] font-extrabold">
         جستجوی پرطرفدار
       </div>
@@ -29,16 +29,17 @@ export default function SearchPage() {
       {searchItems.map((title, index) => (
           <div 
             key={index}
-            className="w-full h-[43px] flex items-center justify-between border-b border-[#00000033]"
+            className="w-full h-[43px] gap-4 flex items-center justify-between"
           >
-            <div className="w-[25px] h-[24px] flex items-center justify-center">
+            <div className="w-[25px] h-[43px] flex items-center justify-center">
               {searchIcon}
             </div>
-            <span className="w-full h-[28px] font-iranYekan text-[16px] font-bold leading-[27.64px] px-6">
+            <span className="w-full h-[43px] content-center font-iranYekan text-[16px] font-bold leading-[27.64px] border-b border-[#00000033]">
               {title}
             </span>
           </div>
         ))}
+      </div>
       </div>
     </div>
   )
